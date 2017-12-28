b588e6e82d28d1f5f40f6e352325b6385c97a825
ashaheed3 <ayanna.shaheed@yahoo.com>
2017-12-27 19:53:39 -0500

    Add files via upload

---
+++ DoubleLinkedLists.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package doublelinkedlists;
+
+/**
+ *
+ * @author LungsSoMuddy
+ */
+public class DoubleLinkedLists {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
+ /*
+LinkList ll = new LinkList();
+        ll
+
+    class LinkList{
+     private Node head;
+     private Node tail;
+     private int size;
+     
+ }
+//constuctors, inserting nodes
+public void iterate
+
+class Node{
+    Node previous;
+    Node next;
+    int date;
+}
+
+public Node(int someInt){
+    this.data = someInt;
+}
+*/
